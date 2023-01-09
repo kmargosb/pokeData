9137e528d6555bd410bc38041a0aae9fb98e1a45
Nelson Camargo <cmargosb@gmail.com>
2023-01-09 02:56:57 +0100

    datos de los pokemones con id random

--- main.js
+++ main.js
@@ -1,11 +1,31 @@
 //  ------------ Variables ------------
 
-const chikorita = 'https://pokeapi.co/api/v2/pokemon/chikorita';
+function randomInt(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+console.log(randomInt(1, 265))
 
+
+document.addEventListener('DOMContentLoaded', function(){
+    const random = randomInt(1, 265);
+    fetchData(random)
+})
 
 // ------------ Fetch ---------------
 
-// fetch(chikorita)
+const fetchData = async (id) => {
+    try{
+        const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+        const data = await res.json()
+        console.log(data)
+    } catch (error) {
+        console.log(error)
+    }    
+}
+
+
+
+
 // .then(response => response.json())
 // .then(data => {
 
