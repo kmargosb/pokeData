1ee975aaacef4e4da016057320b0062ffa7aeb95
Nelson Camargo <cmargosb@gmail.com>
2023-01-09 01:46:25 +0100

    card finish

---
+++ main.js
@@ -0,0 +1,19 @@
+//  ------------ Variables ------------
+
+const chikorita = 'https://pokeapi.co/api/v2/pokemon/chikorita';
+
+
+// ------------ Fetch ---------------
+
+// fetch(chikorita)
+// .then(response => response.json())
+// .then(data => {
+
+//     let $pokeCards = document.querySelector('.card');
+//     $pokeCards.innerHTML = 
+//     `
+//     <h3>${data.name}</h3><p>${data.order}</p>
+//     <img src="${data.sprites.front_shiny}" />
+//     `
+// })
+// .catch(err=>console.log(err))
